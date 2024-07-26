2253f5aebff374a4498a5cce48583a8bdea7d811
M.D.W.De Silva <wdulanjith@gmail.com>
2024-07-26 12:41:06 +0530

    HardwareItemService updated

--- src/main/java/edu/icet/rms/service/HardwareItemService.java
+++ src/main/java/edu/icet/rms/service/HardwareItemService.java
@@ -1,4 +1,21 @@
 package edu.icet.rms.service;
 
+import edu.icet.rms.entity.CustomerEntity;
+import edu.icet.rms.entity.HardwareItemEntity;
+import edu.icet.rms.model.Customer;
+import edu.icet.rms.model.HardwareItem;
+
+import java.util.List;
+
 public interface HardwareItemService {
+
+    HardwareItem persist(HardwareItemEntity hardwareItemEntity);
+
+    List<HardwareItem> retrieveAll();
+
+    HardwareItem update(HardwareItemEntity hardwareItemEntity);
+
+    void deleteById(Long id);
+
+    HardwareItem searchById(Long id);
 }
