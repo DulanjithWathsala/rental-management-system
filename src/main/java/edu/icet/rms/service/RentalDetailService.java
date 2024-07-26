02610df9dffb19cbbed71bd7bf38d548e62656f3
M.D.W.De Silva <wdulanjith@gmail.com>
2024-07-26 12:42:59 +0530

    RentalDetailService updated

--- src/main/java/edu/icet/rms/service/RentalDetailService.java
+++ src/main/java/edu/icet/rms/service/RentalDetailService.java
@@ -1,4 +1,21 @@
 package edu.icet.rms.service;
 
+import edu.icet.rms.entity.HardwareItemEntity;
+import edu.icet.rms.entity.RentalDetailEntity;
+import edu.icet.rms.model.HardwareItem;
+import edu.icet.rms.model.RentalDetail;
+
+import java.util.List;
+
 public interface RentalDetailService {
+
+    RentalDetail persist(RentalDetailEntity rentalDetailEntity);
+
+    List<HardwareItem> retrieveAll();
+
+    RentalDetail update(RentalDetailEntity rentalDetailEntity);
+
+    void deleteById(Long id);
+
+    RentalDetail searchById(Long id);
 }
