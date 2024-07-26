f2eaa1f2a4b27c8103b09ad3c450eb7c09713e71
M.D.W.De Silva <wdulanjith@gmail.com>
2024-07-26 17:34:15 +0530

    initiate exception layer with Global exception handler

---
+++ src/main/java/edu/icet/rms/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package edu.icet.rms.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    
+}
