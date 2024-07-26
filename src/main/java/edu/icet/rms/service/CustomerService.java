bc4a791197c8069223896ac656cb382332c44bf3
M.D.W.De Silva <wdulanjith@gmail.com>
2024-07-26 12:39:20 +0530

    Customer Service updated

--- src/main/java/edu/icet/rms/service/CustomerService.java
+++ src/main/java/edu/icet/rms/service/CustomerService.java
@@ -1,6 +1,19 @@
 package edu.icet.rms.service;
 
+import edu.icet.rms.entity.CustomerEntity;
+import edu.icet.rms.model.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
 
+    Customer persist(CustomerEntity customerEntity);
 
+    List<Customer> retrieveAll();
+
+    Customer update(CustomerEntity customerEntity);
+
+    void deleteById(Long id);
+
+    Customer searchById(Long id);
 }
